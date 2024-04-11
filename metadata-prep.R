cfe7f85e33b3b6cdc8ee952e11ca173c8bc34546
Amanda Regan <amandaregan@MandysMacMini.local>
2024-04-11 09:22:02 -0400

    add readme, save metaprep

--- metadata-prep.R
+++ metadata-prep.R
@@ -4,4 +4,4 @@ files <- list.files("txt/", pattern="*.txt") %>% as.data.frame()
 files <- rename(files, "filename" = ".")
 files$year <- file_path_sans_ext(basename(files$filename))
 files <- separate_wider_delim(files, year, "-", names=c("year", "month", "volume"), too_few="align_start")
-
+write.csv(files, "metadata.csv", row.names = FALSE)
